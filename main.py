87b8d0b69981e3582438783f1aab6080d1bde124
Jay Kadel <94356548+Jay042003@users.noreply.github.com>
2024-03-10 20:52:51 +0530

    final commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from utils import mediapipe_utils
 with open('svm_model.pkl', 'rb') as f:
     loaded_svm_model, loaded_scaler = pickle.load(f)
 
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(0) 
 with mp.solutions.hands.Hands(static_image_mode=True,
                               max_num_hands=2,
                               min_detection_confidence=0.3) as model:
@@ -31,14 +31,20 @@ with mp.solutions.hands.Hands(static_image_mode=True,
                 x = np.array(x).reshape(1, -1)
                 x = loaded_scaler.transform(x)
                 y = loaded_svm_model.predict(x)
-                if y[0] == 'space':
-                    text += '_'
-                elif y[0] == 'del':
-                    if len(y) != 0:
-                        text = text.rstrip(text[-1])
-                else:
-                    text += y[0]
+                if y[0] is not None:
+                    if y[0] == 'space':
+                        text += '_'
+                    elif y[0] == 'del':
+                        if len(y) != 0:
+                            text = text.rstrip(text[-1])
+                    else:
+                        text += y[0]
+                else: 
+                    continue
+
         counter += 1
+        if text[-4:-1] == 'QQQ':
+            break
         cv.putText(img, text, (50, 50),
                    cv.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255))
         cv.imshow('img', img)
